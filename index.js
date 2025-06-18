c9d9e92c7927715d8ad7ce1a6ea21c1c27905add
Sebastian Markbage <sebastian@calyptus.eu>
2025-06-18 18:28:45 -0400

    Missing exports

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export { default as ScrollTimeline } from "./scroll-timeline";
-export { default as ViewTimeline } from "./view-timeline";
-export { default as TouchPanTimeline } from "./touch-pan-timeline";
+export { default as ScrollTimeline } from "./scroll-timeline.js";
+export { default as ViewTimeline } from "./view-timeline.js";
+export { default as TouchPanTimeline } from "./touch-pan-timeline.js";
